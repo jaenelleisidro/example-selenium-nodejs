9192038ded4152257d6ab35fea22e6910209b925
jaenelleisidro <jaenelleisidro@gmail.com>
2021-12-23 22:47:42 +0800

    a working selenium test

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # example-selenium-nodejs
 
+to run, type this
+mocha index.js
+
+
 reference 
 https://www.lambdatest.com/blog/how-to-run-automation-tests-using-selenium-and-nodejs/
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-var webdriver = require('selenium-webdriver');
- 
-var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
- 
-driver.get('http://www.lambdatest.com');
-// driver.quit();
+var assert = require('assert');
+webdriver = require('selenium-webdriver');
+describe('Verify setup with Google Search', function() {
+   it('test case should work', function() {
+      var driver = new webdriver.Builder().
+      withCapabilities(webdriver.Capabilities.chrome()).
+      build();
+      driver.get('http://www.google.com');
+      var searchBox = driver.findElement(webdriver.By.name('q'));
+      searchBox.sendKeys('selenium node automation');
+      searchBox.getAttribute('value').then(function(value) {
+      assert.equal(value, 'selenium node automation');
+      });
+      driver.quit();
+   });
+});
